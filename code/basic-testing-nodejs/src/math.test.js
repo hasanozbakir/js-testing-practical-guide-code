a282b47deace0016ca365f706c12749fdc9986bf
hasanozbakir <hasan.h.ozbakir@gmail.com>
2025-08-24 15:09:42 +0300

    add unit tests for backend

---
+++ code/basic-testing-nodejs/src/math.test.js
@@ -0,0 +1,65 @@
+import { expect, it } from "vitest";
+import { add } from "./math";
+
+it("should summarize all number values inn an array", () => {
+  // Arrange
+  const numbers = [1, 2, 3];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  const expectedValue = numbers.reduce((prev, curr) => prev + curr, 0);
+  expect(result).toBe(expectedValue);
+});
+
+it("should yield NaN if at least one invalid number is provided", () => {
+  const numbers = ["invalid", 1];
+
+  const result = add(numbers);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values are provided", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedValue = numbers.reduce((prev, curr) => +prev + +curr, 0);
+  expect(result).toBe(expectedValue);
+});
+
+it("should yield 0 if an empty array is provided", () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).toBe(0);
+});
+
+it("should throw an error if no value is passed into the function by try-catch block", () => {
+  let result;
+  try {
+    result = add();
+  } catch (error) {
+    expect(result).toBeUndefined();
+  }
+});
+
+it("should throw an error if no value is passed into the function", () => {
+  const resultFn = () => {
+    add();
+  };
+  expect(resultFn).toThrow(/is not iterable/);
+});
+
+it("should throw an error if provide with multiple arguments insread of an array", () => {
+  const num1 = 1;
+  const num2 = 2;
+
+  const resultFn = () => {
+    add(num1, num2);
+  };
+  expect(resultFn).toThrow(/is not iterable/);
+});
